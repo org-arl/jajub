180e9ebb1e6d38c397644785286f2b57b9815ac8
Mandar Chitre <mandar@nus.edu.sg>
2020-06-01 17:47:27 +0800

    feat: add support for exec and get for primitives

--- src/main/java/org/arl/jajub/JuliaBridge.java
+++ src/main/java/org/arl/jajub/JuliaBridge.java
@@ -2,6 +2,7 @@ package org.arl.jajub;
 
 import java.util.*;
 import java.io.*;
+import java.nio.*;
 
 /**
  * Java-Julia bridge.
@@ -13,6 +14,7 @@ public class JuliaBridge {
   protected final static int CR = 10;
   protected final static long TIMEOUT = 10000;
   protected final static long POLL_DELAY = 10;
+  protected final static String TERMINATOR = "\"__##@@##__\"";
 
   protected final String[] JULIA_EXEC = {
     "bin/julia",
@@ -23,7 +25,7 @@ public class JuliaBridge {
     "-iq",
     "--startup-file=no",
     "-e",
-    "using InteractiveUtils; versioninfo()"
+    "using InteractiveUtils; versioninfo(); println("+TERMINATOR+");"
   };
 
   protected ProcessBuilder jbuilder;
@@ -69,15 +71,12 @@ public class JuliaBridge {
     out = julia.getOutputStream();
     while (true) {
       String s = readline(timeout);
-      if (s.length() == 0) {
+      if (s == null) {
         close();
         throw new IOException("Bad Julia process");
       }
-      if (s.startsWith("Julia Version ")) {
-        ver = s;
-        flush();
-        return;
-      }
+      if (s.startsWith("Julia Version ")) ver = s;
+      else if (TERMINATOR.contains(s)) break;
     }
   }
 
@@ -99,11 +98,101 @@ public class JuliaBridge {
   }
 
   public String getJuliaVersion() {
-    if (!isOpen()) throw new IllegalStateException("JuliaBridge not open");
+    openIfNecessary();
     return ver;
   }
 
+  public List<String> exec(String cmd) {
+    openIfNecessary();
+    List<String> rsp = new ArrayList<String>();
+    try {
+      flush();
+      write(cmd + "; " + TERMINATOR);
+      while (true) {
+        String s = readline(TIMEOUT);
+        if (s.equals(TERMINATOR) || s == null) return rsp;
+        rsp.add(s);
+      }
+    } catch (InterruptedException ex) {
+      Thread.currentThread().interrupt();
+    } catch (IOException ex) {
+      throw new RuntimeException("JuliaBridge connection broken", ex);
+    }
+    return rsp;
+  }
+
+  public Object get(String varname) {
+    List<String> rsp = exec("println(typeof("+varname+"))");
+    if (rsp.size() < 1) throw new RuntimeException("Invalid response from Julia REPL");
+    String type = rsp.get(0);
+    if (type.equals("Nothing")) return null;
+    if (type.equals("Missing")) return null;
+    try {
+      if (type.equals("String")) {
+        rsp = exec("println(sizeof("+varname+"))");
+        if (rsp.size() < 1) throw new RuntimeException("Invalid response from Julia REPL");
+        int n = Integer.parseInt(rsp.get(0));
+        write("write(stdout, "+varname+")");
+        byte[] buf = new byte[n];
+        read(buf, TIMEOUT);
+        return new String(buf);
+      }
+      write("write(stdout, "+varname+")");
+      if (type.equals("Int64")) return readNumber(8, false);
+      if (type.equals("Int32")) return readNumber(4, false);
+      if (type.equals("Int16")) return readNumber(2, false);
+      if (type.equals("Int8")) return readNumber(1, false);
+      if (type.equals("Float64")) return readNumber(8, true);
+      if (type.equals("Float32")) return readNumber(4, true);
+      throw new RuntimeException("Unsupported type: "+type);
+    } catch (InterruptedException ex) {
+      Thread.currentThread().interrupt();
+    } catch (IOException ex) {
+      throw new RuntimeException("JuliaBridge connection broken", ex);
+    }
+    return null;
+  }
+
+  public Object call(String func, Object... args) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("_ans_ = ");
+    sb.append(func);
+    sb.append('(');
+    sb.append(')');
+    exec(sb.toString());
+    return get("_ans_");
+  }
+
   ////// private stuff
+
+  protected void openIfNecessary() {
+    if (isOpen()) return;
+    try {
+      open();
+    } catch(IOException ex) {
+      throw new RuntimeException("Unable to open JuliaBridge", ex);
+    }
+  }
+
+  protected Object readNumber(int nbytes, boolean fp) throws IOException, InterruptedException {
+    byte[] buf = new byte[nbytes];
+    read(buf, TIMEOUT);
+    if (nbytes == 1) return buf[0];
+    ByteBuffer bb = ByteBuffer.wrap(buf).order(ByteOrder.nativeOrder());
+    if (fp) {
+      switch (nbytes) {
+        case 4: return bb.asFloatBuffer().get();
+        case 8: return bb.asDoubleBuffer().get();
+      }
+    } else {
+      switch (nbytes) {
+        case 2: return bb.asShortBuffer().get();
+        case 4: return bb.asIntBuffer().get();
+        case 8: return bb.asLongBuffer().get();
+      }
+    }
+    return null;
+  }
 
   protected String getJuliaExec() {
     String jhome = System.getenv("JULIA_HOME");
@@ -155,7 +244,7 @@ public class JuliaBridge {
 
   protected String readline(long timeout) throws IOException, InterruptedException {
     long t = System.currentTimeMillis() + timeout;
-    StringBuffer sb = new StringBuffer();
+    StringBuilder sb = new StringBuilder();
     do {
       while (inp.available() > 0 && !Thread.interrupted()) {
         int b = inp.read();
@@ -164,6 +253,7 @@ public class JuliaBridge {
       }
       Thread.sleep(POLL_DELAY);
     } while (System.currentTimeMillis() < t);
+    if (sb.length() == 0) return null;
     return sb.toString();
   }
 
--- src/test/groovy/org/arl/jajub/JuliaBridgeTest.groovy
+++ src/test/groovy/org/arl/jajub/JuliaBridgeTest.groovy
@@ -15,4 +15,68 @@ class JuliaBridgeTest extends Specification {
       v.startsWith('Julia Version ')
   }
 
+  def "exec Julia code without open"() {
+    setup:
+      def julia = new JuliaBridge()
+    when:
+      def rsp = julia.exec('println(1+2)')
+      julia.close()
+    then:
+      rsp == ['3']
+  }
+
+  def "exec Julia code with open"() {
+    setup:
+      def julia = new JuliaBridge()
+    when:
+      julia.open()
+      def rsp1 = julia.exec('println(1+2)')
+      def rsp2 = julia.exec('1+2')
+      julia.close()
+    then:
+      rsp1 == ['3']
+      rsp2 == []
+  }
+
+  def "get Julia variables"() {
+    setup:
+      def julia = new JuliaBridge()
+    when:
+      julia.open()
+      julia.exec(cmd)
+      def rsp = julia.get('x')
+      julia.close()
+    then:
+      rsp.class == t
+      rsp == v
+    where:
+      cmd              | t         | v
+      'x = Int64(7)'   | Long      | 7
+      'x = Int32(7)'   | Integer   | 7
+      'x = Int16(7)'   | Short     | 7
+      'x = Int8(7)'    | Byte      | 7
+      'x = Float64(7)' | Double    | 7.0
+      'x = Float32(7)' | Float     | 7.0
+      'x = Inf64'      | Double    | Double.POSITIVE_INFINITY
+      'x = -Inf32'     | Float     | Float.NEGATIVE_INFINITY
+      'x = NaN64'      | Double    | Double.NaN
+      'x = "7"'        | String    | "7"
+      'x = "αβγ"'      | String    | "αβγ"
+  }
+
+  def "get Julia null variable"() {
+    setup:
+      def julia = new JuliaBridge()
+    when:
+      julia.open()
+      julia.exec('x = nothing')
+      def rsp1 = julia.get('x')
+      julia.exec('x = missing')
+      def rsp2 = julia.get('x')
+      julia.close()
+    then:
+      rsp1 == null
+      rsp2 == null
+  }
+
 }
